dd323934f0906502e52dc8f7fd9c7a9df22790b2
Limitz4 <qafarlicingiz18@gmail.com>
2025-11-22 09:54:29 +0000

    Add README.md and fixed 0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -4,6 +4,6 @@ def square_matrix_simple(matrix=[]):
     for row in matrix:
         new_row = []
         for num in row:
-            new_row.append(num * num)
+            new_row.append(num ** 2)
         new_matrix.append(new_row)
     return new_matrix
