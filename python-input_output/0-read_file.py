6ffadbeedac7d2e6d83337ad63f4f68d1e9c6827
Limitz4 <qafarlicingiz42@gmail.com>
2025-11-30 11:20:47 +0000

    Fix 0-read_file.py formatting and documentation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-"""
-Function to read and print text file
-"""
+"""Defines a text file-reading function."""
 
 
 def read_file(filename=""):
-    """
-    Reads a text file and prints it to stdout
-    """
-    with open(filename, 'r', encoding='utf-8') as f:
-        print(f.read(), end='')
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
