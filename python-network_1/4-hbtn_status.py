b897fd1fe28ea99cacdb7f9c5ccd6c3bbddf51e0
Limitz4 <qafarlicingiz18@gmail.com>
2025-12-06 15:11:56 +0000

    Create 4-hbtn_status.py script

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -5,9 +5,7 @@ import requests
 
 
 if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
-    response = requests.get(url)
-    
+    r = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
