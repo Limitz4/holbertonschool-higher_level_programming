39386cf785b52b9b297473422c638f233bf10eac
Limitz4 <qafarlicingiz18@gmail.com>
2025-12-06 15:52:30 +0000

    Fix 100-github_commits.py for test repos

--- python-network_1/100-github_commits.py
+++ python-network_1/100-github_commits.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
-"""Lists 10 recent GitHub commits."""
+"""GitHub commits."""
 
 import sys
 import requests
 
 
 if __name__ == "__main__":
-    repo = sys.argv[1]
-    owner = sys.argv[2]
-    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
-    
-    r = requests.get(url, params={'per_page': 10})
-    commits = r.json()
-    
-    if type(commits) is list:
-        for commit in commits:
-            sha = commit.get('sha')
-            name = commit.get('commit', {}).get('author', {}).get('name')
-            print("{}: {}".format(sha, name))
+    r = requests.get("https://api.github.com/repos/{}/{}/commits"
+                     .format(sys.argv[2], sys.argv[1]))
+    j = r.json()
+    for c in j[:10]:
+        print("{}: {}".format(c['sha'], c['commit']['author']['name']))
