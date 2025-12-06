8c25bff2ecd558269ab4623109160059f7f7b1d2
Limitz4 <qafarlicingiz18@gmail.com>
2025-12-06 15:51:02 +0000

    Create 100-github_commits.py script

--- python-network_1/100-github_commits.py
+++ python-network_1/100-github_commits.py
@@ -8,13 +8,13 @@ import requests
 if __name__ == "__main__":
     repo = sys.argv[1]
     owner = sys.argv[2]
-    url = "https://api.github.com/repos/{}/{}/commits"
-    url = url.format(owner, repo)
+    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
     
     r = requests.get(url, params={'per_page': 10})
     commits = r.json()
     
-    for commit in commits:
-        sha = commit.get('sha')
-        author = commit['commit']['author']['name']
-        print("{}: {}".format(sha, author))
+    if type(commits) is list:
+        for commit in commits:
+            sha = commit.get('sha')
+            name = commit.get('commit', {}).get('author', {}).get('name')
+            print("{}: {}".format(sha, name))
