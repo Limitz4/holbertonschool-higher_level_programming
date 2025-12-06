3b9e07303ab893aad9237a9cfc2be41150e3be99
Limitz4 <qafarlicingiz18@gmail.com>
2025-12-06 14:54:16 +0000

    Create 1-hbtn_header.py script

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Takes in a URL, sends a request and displays X-Request-Id header
+"""
+
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    
+    with urllib.request.urlopen(url) as response:
+        print(response.getheader('X-Request-Id'))
