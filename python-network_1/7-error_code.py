c463fb95a08e16592026387dc7cab643e4a5d723
Limitz4 <qafarlicingiz18@gmail.com>
2025-12-06 15:33:47 +0000

    Create 7-error_code.py script

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""Handles HTTP errors with requests."""
+"""HTTP error handler."""
 
 import sys
 import requests
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    r = requests.get(url)
+    r = requests.get(sys.argv[1])
     
     if r.status_code >= 400:
-        print("Error code: {}".format(r.status_code))
+        print("Error code:", r.status_code)
     else:
         print(r.text)
