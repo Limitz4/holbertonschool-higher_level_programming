7dff6b111e77c3f5f98d026cb5e9484e4b644ca7
Limitz4 <qafarlicingiz18@gmail.com>
2025-12-06 16:40:25 +0000

    Add task_01_jinja.py - Flask app with Jinja2 templates

---
+++ python-server_side_rendering/task_01_jinja.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Flask application with Jinja2 templates.
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    """Home page route."""
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    """About page route."""
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    """Contact page route."""
+    return render_template('contact.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
